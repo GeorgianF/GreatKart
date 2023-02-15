bfaa8bcaf77e3fa89e26236a47c40e98b5cf8c38
GeorgianF <georgianfurtuna@yahoo.com>
2023-02-15 09:26:02 +0000

    add context to product detail page

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<slug:category_slug>/', views.store, name='products_by_category'),
     path(
         '<slug:category_slug>/<slug:product_slug>',
-        views.product_details,
-        name='product_details'
+        views.product_detail,
+        name='product_detail'
         ),
 ]
