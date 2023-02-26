1145097812c63a4967ef3d7591d68498a6055187
GeorgianF <georgianfurtuna@yahoo.com>
2023-02-26 09:39:48 +0000

    add functionality to completetly remove item from the cart

--- carts/urls.py
+++ carts/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
         views.remove_cart,
         name='remove_cart'
         ),
+    path(
+        'remove_cart_item/<int:product_id>/',
+        views.remove_cart_item,
+        name='remove_cart_item'
+        ),
 ]
