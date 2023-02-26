4ee1b09a73ee1b93cfb451ed3231ad718a6f7025
GeorgianF <georgianfurtuna@yahoo.com>
2023-02-26 09:24:47 +0000

    add cart items on the the card page

--- carts/views.py
+++ carts/views.py
@@ -39,3 +39,25 @@ def add_cart(request, product_id):
         cart_item.save()
 
     return redirect('cart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = total * 0.02
+        grand_total = total + tax
+    except Exception as e:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/cart.html', context)
