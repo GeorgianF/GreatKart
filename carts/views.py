cfbcaf314316c8ff81e5ccf10eafa49c08d4e137
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2023-04-23 11:38:21 +0200

    fix cart bugs increment/delete

--- carts/views.py
+++ carts/views.py
@@ -27,7 +27,7 @@ def add_cart(request, product_id):
                     variation_value__iexact=value
                     )
                 product_variation.append(variation)
-            except:
+            except Exception as e:
                 pass
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -119,21 +119,32 @@ def cart(request, total=0, quantity=0, cart_items=None):
     return render(request, 'store/cart.html', context)
 
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(
+            product=product,
+            cart=cart,
+            id=cart_item_id
+            )
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except Exception as e:
+        pass
     return redirect('cart')
 
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(
+        product=product,
+        cart=cart,
+        id=cart_item_id
+        )
     cart_item.delete()
     return redirect('cart')
