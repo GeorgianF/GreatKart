e5915304fe678b65d45202c36ffe4bea0ed18a64
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2023-05-01 15:12:41 +0200

    add registration form error handling

--- accounts/forms.py
+++ accounts/forms.py
@@ -35,3 +35,14 @@ class RegistrationForm(forms.ModelForm):
             'password'].widget.attrs['placeholder'] = 'Enter Password'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password is not None and confirm_password is not None:
+            if password != confirm_password:
+                raise forms.ValidationError(
+                    'Password does not match!'
+                )
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 # Create your views here.
 
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Save the user to the database
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            phone_number = form.cleaned_data.get('phone_number')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            username = email.split('@')[0]
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=password,
+                username=username,
+            )
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
 
     context = {
         'form': form,
     }
+
     return render(request, 'accounts/register.html', context)
 
 
