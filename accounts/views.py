9a69bf4a495380dac7e3111a16c4d2e8fc3e3984
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2023-05-02 10:43:01 +0200

    add registration form messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
+from django.contrib import messages
 
 # Create your views here.
 
@@ -25,6 +26,8 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
+            messages.success(request, 'Registration successful. Please login.')
+            return redirect('register')
     else:
         form = RegistrationForm()
 
