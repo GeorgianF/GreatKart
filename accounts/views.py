b94b5138d21c0ffc36caf46056d1f0c53139dabb
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2023-06-05 10:55:19 +0200

    add function for non logged users to save cart

--- accounts/views.py
+++ accounts/views.py
@@ -86,10 +86,36 @@ def login(request):
                     cart=cart
                     ).exists()
                 if is_cart_item_exists:
+                    print('cart item exists')
                     cart_item = CartItem.objects.filter(cart=cart)
+                    # Get the product variations
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variation.all()
+                        product_variation.append(list(variation))
+                    # Get the cart items from the user to access his product
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variation.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
             except Cart.DoesNotExist:
                 pass
             auth.login(request, user)
